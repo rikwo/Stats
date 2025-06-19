03b23442033340b1dd758573496ac4084c1b2792
wkk4 <ricky.kwong95@gmail.com>
2025-06-19 14:52:46 -0700

    Initial Commit

---
+++ Stats/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Stats
+//
+//  Created by Ricky Kwong on 2025-06-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Stats/StatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StatsApp.swift
+//  Stats
+//
+//  Created by Ricky Kwong on 2025-06-19.
+//
+
+import SwiftUI
+
+@main
+struct StatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
