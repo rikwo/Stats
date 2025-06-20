d6ee33cb69a4e0b1d9be6c4c2be6b0c38201bec5
wkk4 <ricky.kwong95@gmail.com>
2025-06-20 11:34:56 -0700

    Added editing functionality

---
+++ Stats/EditGameView.swift
@@ -0,0 +1,18 @@
+//
+//  EditGameView.swift
+//  Stats
+//
+//  Created by Ricky Kwong on 2025-06-19.
+//
+
+import SwiftUI
+
+struct EditGameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditGameView()
+}
---
+++ Stats/Stat.swift
@@ -0,0 +1,8 @@
+//
+//  Stat.swift
+//  Stats
+//
+//  Created by Ricky Kwong on 2025-06-20.
+//
+
+import Foundation
